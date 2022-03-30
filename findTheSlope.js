6d482dfabccc97e0c1e682e31666162e798c46ef
Brian Regehr <brian.regehr@gmail.com>
2022-03-29 19:29:20 -0700

    codewars

---
+++ findTheSlope.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/55a75e2d0803fea18f00009d/train/javascript
+
+// Given an array of 4 integers
+// [a,b,c,d] representing two points (a, b) and (c, d), return a string representation of the slope of the line joining these two points.
+
+// For an undefined slope (division by 0), return undefined . Note that the "undefined" is case-sensitive.
+
+//    a:x1
+//    b:y1
+//    c:x2
+//    d:y2
+// Assume that [a,b,c,d] and the answer are all integers (no floating numbers!). Slope: https://en.wikipedia.org/wiki/Slope
+
+// m= {y_2-y_1} / {x_2-x_1}.
+
+function slope(points) {
+  return points[2] - points[0] === 0
+    ? 'undefined'
+    : Math.round((points[3] - points[1]) / (points[2] - points[0])).toString();
+}
