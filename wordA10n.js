793a29897821c542bcfe6a47e59f5c1626d57055
Brian Regehr <brian.regehr@gmail.com>
2022-08-23 17:13:50 -0700

    codewars

---
+++ wordA10n.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/5375f921003bf62192000746/train/javascript
+
+// The word i18n is a common abbreviation of internationalization in the developer community, used instead of typing the whole word and trying to spell it correctly. Similarly, a11y is an abbreviation of accessibility.
+
+// Write a function that takes a string and turns any and all "words" (see below) within that string of length 4 or greater into an abbreviation, following these rules:
+
+// A "word" is a sequence of alphabetical characters. By this definition, any other character like a space or hyphen (eg. "elephant-ride") will split up a series of letters into two words (eg. "elephant" and "ride").
+// The abbreviated version of the word should have the first letter, then the number of removed characters, then the last letter (eg. "elephant ride" => "e6t r2e").
+// Example
+// abbreviate("elephant-rides are really fun!")
+// //          ^^^^^^^^*^^^^^*^^^*^^^^^^*^^^*
+// // words (^):   "elephant" "rides" "are" "really" "fun"
+// //                123456     123     1     1234     1
+// // ignore short words:               X              X
+
+// // abbreviate:    "e6t"     "r3s"  "are"  "r4y"   "fun"
+// // all non-word characters (*) remain in place
+// //                     "-"      " "    " "     " "     "!"
+// === "e6t-r3s are r4y fun!"
+
+function abbreviate(string) {
+  let arr = string
+    .replace(/-/g, ' - ')
+    .replace(/!/g, ' !')
+    .replace(/,/g, ' ,')
+    .split(' ')
+    .map((el) => {
+      if (el.length > 3) {
+        let len = el.length - 2;
+        return `${el.slice(0, 1)}${len}${el.slice(-1)}`;
+      } else {
+        return el;
+      }
+    });
+  return arr
+    .join(' ')
+    .replace(/ - /g, '-')
+    .replace(/ !/g, '!')
+    .replace(/ ,/g, ',');
+}
