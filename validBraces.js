818beb83ce75529f3dc9c6b7e0dbf174c127ecf7
Brian Regehr <brian.regehr@gmail.com>
2023-07-01 14:31:14 -0700

    codewars

---
+++ validBraces.js
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/5277c8a221e209d3f6000b56/train/javascript
+
+function validBraces(braces) {
+  let stack = [];
+  let pairObj = { ')': '(', ']': '[', '}': '{' };
+  let inputArr = braces.split('');
+
+  for (let i = 0; i < inputArr.length; i++) {
+    if (
+      (inputArr[i] === ')' || inputArr[i] === ']' || inputArr[i] === '}') &&
+      stack.length === 0
+    ) {
+      return false;
+    }
+    if (
+      (inputArr[i] === ')' || inputArr[i] === ']' || inputArr[i] === '}') &&
+      stack[stack.length - 1] !== pairObj[inputArr[i]]
+    ) {
+      return false;
+    }
+    if (inputArr[i] === '(' || inputArr[i] === '[' || inputArr[i] === '{') {
+      stack.push(inputArr[i]);
+    }
+    if (inputArr[i] && stack[stack.length - 1] === pairObj[inputArr[i]]) {
+      stack.pop();
+    }
+  }
+  return stack.length === 0 ? true : false;
+}
