4449c2016a8ba8728b02ad8c76ac33aff3f974ae
Brian Regehr <brian.regehr@gmail.com>
2023-07-04 17:04:48 -0700

    codewars

---
+++ theHashtagGenerator.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/52449b062fb80683ec000024/train/javascript
+
+function generateHashtag(str) {
+  let strArr = str
+    .split(' ')
+    .filter((word) => word !== '')
+    .map((word) => word[0].toUpperCase().concat(word.slice(1)))
+    .join('');
+  return strArr.length > 140 || strArr.length < 1 ? false : `#${strArr}`;
+}
