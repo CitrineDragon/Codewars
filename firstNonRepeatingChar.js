844334a6a7f195348f417d18f25a2954e43623ea
Brian Regehr <brian.regehr@gmail.com>
2023-07-15 13:38:34 -0700

    codewars

---
+++ firstNonRepeatingChar.js
@@ -0,0 +1,34 @@
+// https://www.codewars.com/kata/52bc74d4ac05d0945d00054e/train/javascript
+
+//This is more performant for larger inputs
+function firstNonRepeatingLetter(s) {
+  const charCounts = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i].toLowerCase();
+    const count = charCounts.get(char) || 0;
+    charCounts.set(char, count + 1);
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i].toLowerCase();
+    if (charCounts.get(char) === 1) {
+      return s[i];
+    }
+  }
+
+  return '';
+}
+
+// function firstNonRepeatingLetter(s) {
+//   const lowercase = s.toLowerCase();
+//   for (let i = 0; i < s.length; i++) {
+//     if (
+//       i === lowercase.lastIndexOf(lowercase[i]) &&
+//       i === lowercase.indexOf(lowercase[i])
+//     ) {
+//       return s[i];
+//     }
+//   }
+//   return '';
+// }
