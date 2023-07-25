0f3baa383933df9872fc2f004550fb0f4de86dc8
Brian Regehr <brian.regehr@gmail.com>
2023-07-25 11:51:23 -0700

    codewars

--- theOfficeBoredomScore.js
+++ theOfficeBoredomScore.js
@@ -14,13 +14,23 @@ function boredom(staff) {
     'pissing about': 25,
   };
 
-  const keys = Object.keys(staff);
-  const totalScore = keys
-    .map((val) => deptScores[staff[val]])
-    .reduce((t, c) => t + c, 0);
-  return totalScore > 99
+  // const keys = Object.keys(staff);
+  // const totalScore = keys
+  //   .map((val) => deptScores[staff[val]])
+  //   .reduce((t, c) => t + c, 0);
+  // return totalScore > 99
+  //   ? 'party time!!'
+  //   : totalScore < 81
+  //   ? 'kill me now'
+  //   : 'i can handle this';
+
+  const score = Object.keys(staff).reduce(
+    (t, c) => t + deptScores[staff[c]],
+    0
+  );
+  return score > 99
     ? 'party time!!'
-    : totalScore < 81
+    : score < 81
     ? 'kill me now'
     : 'i can handle this';
 }
